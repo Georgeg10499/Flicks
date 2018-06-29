1bedd4b01329ba956aef99fe4db800b69ced7447
Jorge Garcia Ruiz <jorge10gr@fb.com>
2018-06-28 17:03:19 -0700

    Initial commit

---
+++ app/src/main/java/com/georgeg10499/flickster/models/Movie.java
@@ -0,0 +1,35 @@
+package com.georgeg10499.flickster.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Movie {
+    //values
+    private String title;
+    private String overview;
+    private String posterPath; // only the path
+
+    //initialize from JSON data
+    public Movie(JSONObject object) throws JSONException{
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+}
+
+
+
+
+
